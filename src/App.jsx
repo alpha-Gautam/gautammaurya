853b87655c2eda5c7209a38703ce45092b7b60b1
Gautam Maurya <gautammaurya1299@gmail.com>
2025-10-04 19:49:16 +0530

    Fix import case for Header component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
