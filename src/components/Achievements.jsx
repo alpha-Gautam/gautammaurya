1266f5ee6578d73a12494ef7ffcb78156ea12062
Gautam Maurya <gautammaurya1299@gmail.com>
2025-10-09 22:40:58 +0530

    Add AchievementsIcons component and update Achievements layout for improved presentation

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { AchievementsIcons } from "../assets/icons";
 
 function Achievements() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-white/10 pt-20">
       <div className="text-[50px] font-bold">Achievements</div>
 
-      <div className="flex flex-col p-10 mt-10 w-[50%] min-h-50% shadow-2xl rounded-lg bg-white/20">
+      <div className="flex flex-col items-center p-10 mt-10 w-[50%] min-h-50% shadow-2xl rounded-lg bg-white/20">
+        <div className="flex justify-center  mb-5">
+          <span>
+            <AchievementsIcons />
+          </span>
+        </div>
         <span className="text-[20px] font-semibold mb-5">
-          Qualified GATE 2025 DA(Data Science AI) — Score: 319, Rank: 10928
+          🏅 Qualified GATE 2025 DA(Data Science AI) — Score: 319, Rank: 10928
         </span>
         <span className="text-[20px] font-semibold mb-5">
-          1st Rank — Institute-level coding contest ’Ode 2 Code’, Jan 2024
+          🏅 1st Rank — Institute-level coding contest ’Ode 2 Code’, Jan 2024
         </span>
         <span className="text-[20px] font-semibold mb-5">
-          Solved 300+ problems across (leetcodegfg, hackerrank, , etc).
+          🏅 Solved 300+ problems across (leetcodegfg, hackerrank, , etc).
         </span>
       </div>
     </div>
