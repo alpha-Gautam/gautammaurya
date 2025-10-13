68b0e4b1daa57392f1ca77b1f85d74ba686fcf26
Gautam Maurya <gautammaurya1299@gmail.com>
2025-10-13 21:16:37 +0530

    Update Skills component layout and styling for improved visual consistency

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,14 +21,14 @@ function Skills() {
   return (
     <div className="min-h-screen flex justify-center px-4 bg-gray-800">
       <div className="w-full max-w-4xl flex flex-col gap-10 sm:gap-20 justify-center items-center">
-        <div className="text-3xl sm:text-4xl md:text-5xl font-bold p-4 sm:p-10 text-center pt-20">
+        <div className="text-3xl sm:text-4xl md:text-5xl font-bold p-4 sm:p-20 text-center pt-20">
           Skills
         </div>
-        <div className="flex rounded-2xl justify-center items-center p-2 mb-20 sm:mb-40 flex-wrap w-full h-full gap-2">
+        <div className="flex flex-wrap justify-center items-center rounded-2xl  p-2 mb-20 sm:mb-40 w-[80%] h-[80%] bg-white/10">
           {skills.map((skill, index) => (
             <span
               key={index}
-              className="bg-gray-800 size-fit px-3 py-1 m-1 rounded-full text-sm hover:scale-110 hover:bg-white/40"
+              className="bg-gray-800 size-fit px-2 py-1 rounded-2xl text-sm hover:scale-110 hover:bg-white/40 m-1"
             >
               {skill}
             </span>
